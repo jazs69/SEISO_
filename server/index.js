afcb36467f19f2c4bf645a15fcc1af0941347177
Jayesh <momsjayesh@gmail.com>
2025-05-18 11:41:14 +0530

    updated server/index.js

--- server/index.js
+++ server/index.js
@@ -48,6 +48,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
