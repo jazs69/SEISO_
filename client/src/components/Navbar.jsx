91ed2389240bc2c1aaa09f69ff8c60d078c7f473
Jayesh <momsjayesh@gmail.com>
2025-05-19 08:48:38 +0530

    updated navbar home.jsx landing page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="fixed w-full top-0 left-0 text-white px-[3vw] xl:px-[8vw] pt-8 xl:py-10 flex justify-between items-center bg-[#151515]">
+    <nav className="fixed w-full top-0 left-0 text-white px-[3vw] xl:px-[8vw] py-10 flex justify-between items-center bg-[#151515]">
       {/* Logo */}
-      <div className="w-52 xl:w-1/5">
+      <div className="w-56 xl:w-1/5">
         <img src={SeisoLogo} alt="" />
       </div>
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,26 +7,30 @@ function Home() {
   return (
     <div>
       <Navbar />
-      <div className="w-full h-[100svh] min-h-[735px] max-h-[1000px] bg-midnight pt-[150px] sm:pt-[180px] text-white antialiased">
-        <div className="container mx-auto pb-12 px-[1rem]">
-          <p className="font-mono tracking-wider leading-[1.6em] text-[18px] md:w-[50%] font-light max-w-[600px]">
-            Seiso rewards you for the responsible disposal of your waste,
-            Creating waste into opportunity.
-          </p>
-          <p className="mt-[10%] sm:mt-[46px] text-[45px] md:text-[80px] lg:text-[98px] leading-snug sm:leading-[1.2em] w-[80%] sm:w-[100%] lg:max-w-[1024px]">
-            Use Seiso & Earn. One Bin at a time.
-          </p>
-          <div className="flex w-[180px] sm:w-[270px] h-[50px] lg:h-[65px] border-[white] border-t-[2px] sm:border-t-[3px] mt-[20%] sm:mt-[90px] group cursor-pointer">
-            <Link
-              to="/verify-waste"
-              className="flex items-center justify-baseline gap-3 hover:gap-7 self-end text-[18px] sm:text-[28px] transition-all duration-300"
-            >
-              <div>Verify waste</div>
-              <ChevronRight size={28} />
-            </Link>
+      <main className="relative w-full h-full">
+        <div className="bg-white">
+          <div className="w-full h-[100svh] min-h-[735px] max-h-[1000px] bg-midnight pt-[150px] sm:pt-[200px] text-white antialiased">
+            <div className="container mx-auto pb-12 px-[1rem]">
+              <p className="font-mono tracking-wider leading-[1.75em] text-[18px] md:w-[50%] font-light max-w-[600px]">
+                Seiso rewards you for the responsible disposal of your waste,
+                Creating waste into opportunity.
+              </p>
+              <p className="mt-[10%] sm:mt-[46px] text-[45px] md:text-[80px] lg:text-[98px] leading-snug sm:leading-tight w-[80%] sm:w-[100%] lg:max-w-[1024px]">
+                Use Seiso & Earn. One Bin at a time.
+              </p>
+              <div className="flex w-[180px] sm:w-[270px] h-[50px] lg:h-[65px] border-[white] border-t-[2px] sm:border-t-[3px] mt-[20%] sm:mt-[100px] group cursor-pointer">
+                <Link
+                  to="/verify-waste"
+                  className="flex items-center justify-baseline gap-3 hover:gap-7 self-end text-[18px] sm:text-[28px] transition-all duration-300"
+                >
+                  <div>Verify waste</div>
+                  <ChevronRight size={28} />
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
